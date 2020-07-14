0bc69cac23027084b6ed754aa718044b6669302c
aditya chhajer <chhajeraditya@gmail.com>
2020-07-14 14:17:18 +0530

    Add files via upload

---
+++ Same Tree  .py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p == None and q == None:
+            return True
+        else:
+            if (p == None and q != None) or (p != None and q == None):
+                return False
+            if p.val == q.val:
+                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+            else:
+                return False
+
+
